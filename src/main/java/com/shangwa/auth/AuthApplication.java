6612a48eb25ec12c53517ba388fb0c9e47a065ba
= <=>
2024-04-15 10:41:15 +0200

    removed unused imports

--- src/main/java/com/shangwa/auth/AuthApplication.java
+++ src/main/java/com/shangwa/auth/AuthApplication.java
@@ -2,10 +2,8 @@ package com.shangwa.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.shangwa.auth.entity.User;
 
