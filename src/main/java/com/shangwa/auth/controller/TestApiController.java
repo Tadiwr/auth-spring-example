3afed579c1d898c39ec7e1f690a00f865fdb4d08
= <=>
2024-04-16 10:15:18 +0200

    made some improvements

--- src/main/java/com/shangwa/auth/controller/TestApiController.java
+++ src/main/java/com/shangwa/auth/controller/TestApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TestApiController {
     
     @GetMapping("/api/getUserInfo")
